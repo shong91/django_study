6d18f58a1887cf547888a5b2ce06ca54e30f3f8e
Soyeon Hong <52073383+hhhongso@users.noreply.github.com>
2020-09-02 23:24:51 +0900

    Initial commit

---
+++ first-django/first/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+def index(request):
+    return HttpResponse("Hello world!")
+
+
+def select(request):
+    message = "수 하나를 입력해주세요. "
+    return HttpResponse(message)
+
+
+def result(request):
+    message = "추첨 결과입니다. "
+    return HttpResponse(message)
