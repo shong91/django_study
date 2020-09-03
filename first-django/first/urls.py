d9cf45eef3c2947f8c000d3fda8246890ebac8a0
Soyeon Hong <52073383+hhhongso@users.noreply.github.com>
2020-09-03 23:10:55 +0900

    url mapping, templates

--- first-django/first/urls.py
+++ first-django/first/urls.py
@@ -1,7 +1,17 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("select/", views.select, name="select"),
     path("result/", views.result, name="result"),
+
+    # 정규표현식도 가능
+    # re_path(r'^select/(?P<year>[0-9]{4}$)', views.select, name="select"),
+    # path('select/<int:year>/', views.select, name="select")
+    # <slug:>
+    # <str:> 모든 문자열
 ]
+
+# path parameter: /{aaa}/ (=pathVariable)
+# query parameter: /?id=aaa (GET)
+# form parameter: / (POST)
--- first-django/first/views.py
+++ first-django/first/views.py
@@ -1,17 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from datetime import datetime
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello world!")
+    # django.shortcuts 을 이용하면 좀 더 간단하다. (template 가져오기, HttpResponse return 을 간략히 할 수 있음)
+    # template = loader.get_template("index.html")
+
+    now = datetime.now()
+    # 동적으로 view 메서드에서 정리한 변수를 삽입
+    context = {
+                'current_date': now,
+    }
+
+    return render(request, 'first/index.html', context)
+    # return HttpResponse(template.render(context, request))
 
 
-def select(request):
-    message = "수 하나를 입력해주세요. "
-    return HttpResponse(message)
+def select(request): # , year
+    context = {'number': 5}
+    return render(request, 'first/select.html', context)
 
 
 def result(request):
-    message = "추첨 결과입니다. "
-    return HttpResponse(message)
+    context = {'numbers': [1,2,3,4,5,6]}
+    return render(request, 'first/result.html', context)
