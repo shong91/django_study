9abac636a46dd96787d51022ee3b0d7ba46cfd90
Soyeon Hong <52073383+hhhongso@users.noreply.github.com>
2020-09-08 23:47:30 +0900

    update views.py

--- first-django/third/views.py
+++ first-django/third/views.py
@@ -10,8 +10,19 @@ def list(request):
     page = request.GET.get('page') # /third/list/?page=1
     items = paginator.get_page(page)
 
+    # Reference: Django official Document (https://docs.djangoproject.com/en/3.1/topics/pagination/)
+    print('count', paginator.count)
+    print('num_pages', paginator.num_pages)
+    page1 = paginator.page(1)
+    print('has_next()', page1.has_next())
+    print('has_previous()', page1.has_previous())
+    print('has_other_pages()', page1.has_other_pages())
+    print('next_page_number()', page1.next_page_number())
+
+
     context = {
-        'restaurants': items
+        'restaurants': items,
+        'paginator': paginator
     }
     return render(request, 'third/list.html', context)
 
