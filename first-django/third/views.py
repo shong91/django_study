396e87e8d9ffd7148ee2b8fa0ce5e33b5510be85
Soyeon Hong <52073383+hhhongso@users.noreply.github.com>
2020-09-10 22:16:13 +0900

    CRUD - C, R(detail), U, D

--- first-django/third/views.py
+++ first-django/third/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from third.models import Restaurant
 from django.core.paginator import Paginator
-
+from third.forms import RestaurantForm
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 def list(request):
@@ -26,3 +27,42 @@ def list(request):
     }
     return render(request, 'third/list.html', context)
 
+
+def create(request):
+    if request.method == 'POST':
+        form = RestaurantForm(request.POST)
+        if form.is_valid():
+            new_item = form.save()
+        return HttpResponseRedirect('/third/list/')
+
+    form = RestaurantForm()
+    return render(request, 'third/create.html', {'form': form})
+
+
+def update(request):
+    if request.method == 'POST' and 'id' in request.POST: # 수정 로직
+        # item = Restaurant.objects.get(pk=request.POST.get('id'))
+        item = get_object_or_404(Restaurant, pk=request.POST.get('id'))
+        form = RestaurantForm(request.POST, instance=item)
+        if form.is_valid():
+            item = form.save()
+    elif request.method == 'GET': # 수정 form
+        # item = Restaurant.objects.get(pk=request.GET.get('id')) # /third/update/?id=$$$
+        item = get_object_or_404(Restaurant, pk=request.GET.get('id'))
+        form = RestaurantForm(instance=item)
+        return render(request, 'third/update.html', {'form': form})
+    return HttpResponseRedirect('/third/list/')
+
+
+def detail(request):
+    if 'id' in request.GET:
+        item = get_object_or_404(Restaurant, pk=request.GET.get('id'))
+        return render(request, 'third/detail.html', {'item': item})
+    return HttpResponseRedirect('/third/list/')
+
+
+def delete(request):
+    if 'id' in request.GET:
+        item = get_object_or_404(Restaurant, pk=request.GET.get('id'))
+        item.delete()
+    return HttpResponseRedirect('/third/list/')
