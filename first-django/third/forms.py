baa7dec552b9e25b05066003b95bf4ec6b2cc3e6
Soyeon Hong <52073383+hhhongso@users.noreply.github.com>
2020-09-13 18:53:09 +0900

    update Model, ModelForm - add password, image url

--- first-django/third/forms.py
+++ first-django/third/forms.py
@@ -34,18 +34,38 @@ class ReviewForm(ModelForm):
 class RestaurantForm(ModelForm):
     class Meta:
         model = Restaurant
-        fields = ['name', 'address']
+        fields = ['name', 'address', 'password', 'image']
         labels = {
             'name': _('이름'),
             'addresss': _('주소'),
+            'password': _('게시물 비밀번호'),
+            'image': _('이미지 url'),
 
         }
         help_texts = {
             'name': _('이름을 입력해주세요. '),
             'address': _('주소를 입력해주세요. '),
+            'password': _('게시물 비밀번호를 입력해주세요. '),
+            'image': _('이미지 url을 입력해주세요.'),
+        }
+        widgets = {
+            'password': forms.PasswordInput()
         }
         error_messages = {
             'name': {
                 'max_length': _('이름이 너무 깁니다! ')
+            },
+            'password': {
+                'max_length': _('패스워드가 너무 깁니다! ')
+            },
+            'image': {
+                'max_length': _('이미지 주소의 길이가 너무 깁니다! ')
             }
         }
+
+
+class UpdateRestaurantForm(RestaurantForm):
+    class Meta:
+        model = Restaurant
+        # 이 form 을 사용할 때, password 는 제외하고 update 함
+        exclude = ['password']
--- first-django/third/models.py
+++ first-django/third/models.py
@@ -7,6 +7,10 @@ class Restaurant(models.Model):
     name = models.CharField(max_length=30)
     address = models.CharField(max_length=200)
 
+    # default 를 지정하여 기존에 password 가 없던 데이터들은 null으로 set된다. (null=True 를 설정하지 않을 경우, 기본적으로 NOT NULL임 )
+    password = models.CharField(max_length=20, default=None, null=True)
+    image = models.CharField(max_length=500, default=None, null=True)
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
